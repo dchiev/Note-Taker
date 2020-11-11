0b328066f8065c9cefb276a1ba98cf4195cb1bd1
Dan Chiev <danchiev2@Gmail.com>
2020-11-10 21:28:03 -0500

    added post route

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,18 +1,28 @@
 const path = require("path");
 const fs = require("fs");
-module.exports = function(app) {
+module.exports = function (app) {
 
-/* var userNotes = fs. readFileSync(".db/db.json", "utf-8");
-if (userNotes) {
-    var pastNotes = JSON.parse(userNotes);
-    notes = pastNotes;
-}
-else {
-    notes = [];
-}
+
+
+    // read JSON
+    app.get("/api/notes", function (req, res) {
+        fs.readFile("./db/db.json", 'utf-8', function (err, data) {
+            res.json(JSON.parse(data));
+            console.log(data)
+        });
+    })
+};
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+
+/* // Save Notes
+app.post("/api/notes", function (req, res) {
+    notes.push(req.body);
+    console.log(notes);
+    res.json(req.body);
+})
  */
-app.get("/api/notes", function (req,res){
-    fs.readFile("./db/db.json", 'utf-8',function(err, data){
-        res.json(JSON.parse(data));
-    });
-})};
+
