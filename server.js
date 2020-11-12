b08b178266aead2fec7677ad26dde227b1f5e3a1
Dan Chiev <danchiev2@Gmail.com>
2020-11-11 20:11:18 -0500

    package start

--- server.js
+++ server.js
@@ -49,11 +49,11 @@ app.delete("/api/notes/:id", function (req, res) {
     var id = req.params.id;
     readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
         .then(function (data) {
-            var notes = JSON.parse(data);
+            let notes = JSON.parse(data);
             notes.splice(id, 1);
             writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(notes))
-            
-                
+                .then(function () {
+                })
         })
     res.json(id);
 });
