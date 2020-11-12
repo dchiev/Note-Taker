1b8b45969d74b0947be3d514a3937ad158bac700
Dan Chiev <danchiev2@Gmail.com>
2020-11-11 19:04:58 -0500

    added gitignore and removed route folders

--- server.js
+++ server.js
@@ -1,21 +1,68 @@
 var express = require("express");
 var path = require("path");
-const fs = require("fs");
-
-
+var util = require('util');
+var fs = require('fs');
 var app = express();
+
 var PORT = process.env.PORT || 5000;
-app.use(express.static('public'));
 
-
+const writefileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+let notes;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
 
 
-require("./routes/api_routes")(app);
-require("./routes/html_routes")(app);
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-app.listen(PORT,function(){
-    console.log("App listening on PORT " + PORT);
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function (data) {
+            return res.json(JSON.parse(data));
+        })
+});
+
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function (data) {
+            notes = JSON.parse(data);
+            if (newNote.id || newNote.id === 0) {
+                let c = notes[newNote.id];
+                nowNote.title = newNote.title;
+                nowNote.text = newNote.text;
+            } else {
+                notes.push(newNote);
+            }
+            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(notes))
+                .then(function () {
+                })
+        });
+    res.json(newNote);
+})
+
+app.delete("/api/notes/:id", function (req, res) {
+    var id = req.params.id;
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function (data) {
+            notes = JSON.parse(data);
+            notes.splice(id, 1);
+            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(notes))
+                .then(function () {
+                })
+        })
+    res.json(id);
+});
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
